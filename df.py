7e5aad801c551d6254a685b1838dd1778280251e
Sergey Batukov <89929657+sashachepuha@users.noreply.github.com>
2021-11-11 17:28:41 +0300

    Update df.py

--- df.py
+++ df.py
@@ -1,14 +1,56 @@
 import requests
 import csv
+import datetime
+import time
+import os
 
-res = requests.get('https://api.exmo.com/v1.1/candles_history?symbol=BTC_USD&resolution=30&from=1580556979&to=1585557979')
-data = res.json()['candles']
+NAME = 'df'
+NOW = round(datetime.datetime.now().timestamp()) # конечное время (текушее время в секундах)
+SIZE = 30 # размер датасета
+QUERY_SIZE = 10 # сколько заберать за раз, API  имеет ограничение в 3000
+TIMEOUT = 1 # задержка между запросами (секунды)
+RESOLUTION = 30 # 1, 5, 15, 30, 45, 60, 120, 180, 240 интервал между значениями (свеча) (минуты)
+SYMBOL = 'BTC_USD' # пара
 
-with open('df.csv', 'w') as csvfile:
+DATA = []
+
+RANGE = int(SIZE / QUERY_SIZE)
+OFFSET = RESOLUTION * 60 * QUERY_SIZE
+FROM = NOW - OFFSET
+TO = NOW
+for i in range(RANGE):
+  res = requests.get('https://api.exmo.com/v1.1/candles_history', params = {
+    'symbol': SYMBOL,
+    'resolution': RESOLUTION,
+    'from': FROM,
+    'to': TO
+  })
+  result = res.json()['candles']
+
+  # обрезка лишних нулей
+  for item in result:
+    item['t'] = int(str(item['t'])[:-3])
+
+  DATA = result + DATA
+
+  # новый временной интервал
+  FROM = DATA[0]['t'] - OFFSET
+  TO = DATA[0]['t']
+
+  time.sleep(TIMEOUT)
+  os.system('clear')
+  print(f'Получено строк: {(i+1)*QUERY_SIZE} из {SIZE}')
+  print(f'Выполнено запросов: {i+1} из {RANGE}')
+
+# запись в csv
+with open(f'{NAME}.csv', 'w') as csvfile:
     fieldnames = ['t','v', 'c', 'o', 'l', 'h']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
-    writer.writerows(data)
+    writer.writerows(DATA)
 
-print("writing complete")
+print('Датасет собран.')
+print(f'Имя: {NAME}.csv')
+print(f'Количество строк: {len(DATA) - 1}')
+print(f'Время выполнения: {round(NOW - datetime.datetime.now().timestamp())} секунд.')
